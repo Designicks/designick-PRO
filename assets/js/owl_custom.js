fbf15f5eb5fd803f48300d0b25e75ae2d609a05e
Nikunj Gurjar <39230292+Designicks@users.noreply.github.com>
2020-05-28 22:55:27 +0530

    first

---
+++ assets/js/owl_custom.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+        $('.clients-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        nav:true,
+        center: true,
+        dots: false,
+        dotsData:false,
+        dotsEach:false,
+        autoplayHoverPause:true,
+        smartSpeed:2000,
+        fluidSpeed:100,
+        slideSpeed: 8000,
+        rewind:false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+ });
+
+
+
+        $('.solution-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        nav:true,
+        center: false,
+        dots: false,
+        dotsData:false,
+        dotsEach:false,
+        autoplayHoverPause:true,
+        smartSpeed:1000,
+        fluidSpeed:100,
+        slideSpeed: 5000,
+        rewind:false,
+        mouseDrag:true,
+        touchDarg:true,
+        lazyload:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+
