fe72b50dec25a31786f0c38735e58d4ce7348fa7
Nikunj Gurjar <39230292+Designicks@users.noreply.github.com>
2020-06-01 18:31:38 +0530

    Portfolio Section Complete

--- assets/js/owl_custom.js
+++ assets/js/owl_custom.js
@@ -33,3 +33,60 @@
         }
     });
 
+
+$(document).ready(function() {
+  $('.port-mobile-owl').owlCarousel({
+     items: 3,
+    loop: true,              
+    center: true,
+    dots: false,
+    nav: true,
+    margin:20,
+    autoplayHoverPause:true,
+    smartSpeed:2000,
+    fluidSpeed:100,
+    slideSpeed: 8000,
+    rewind:false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:4
+      }
+    }
+ });
+
+  $('.port-web-owl').owlCarousel({
+     items: 3,
+    loop: true,              
+    center: true,
+    dots: false,
+    nav: true,
+    margin:70,
+    autoplayHoverPause:true,
+    smartSpeed:2000,
+    fluidSpeed:100,
+    slideSpeed: 8000,
+    rewind:false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:2
+      }
+    }
+ });
+
+});
